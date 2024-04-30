63de41df64e64e9e0b5dec9498953f577733cb0f
Souza Allan <allansou@lx2-870-27966.ad.helsinki.fi>
2024-04-30 16:09:46 +0300

    Extracting the information from EnvThes to tabular data.

--- code/R-data-processing.R
+++ code/R-data-processing.R
@@ -49,45 +49,105 @@ toc()
 #### View the results
 term_query
 
-#### Defining a general SPARQL query (all terms from the vocabulary: term and definition) ----
-query <- '
+## one by one approach ----
+### SPARQL query to retrieve terms and labels ----
+query_labels <- '
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX dc: <http://purl.org/dc/terms/>
 
-SELECT ?term ?prefLabel ?definition
+SELECT ?term ?prefLabel
 WHERE {
     ?term skos:prefLabel ?prefLabel .
-    OPTIONAL { ?term skos:definition ?definition }
-}
-'
-
-# NEW GENERAL QUERY (2024-04-29: currently taking too long to run - still needs to be tested to see if the code works)
-#### Defining a general SPARQL query (all terms from the vocabulary: columns of interest) ----
-query <- '
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX dc: <http://purl.org/dc/terms/>
-
-SELECT ?term ?prefLabel ?altLabel ?altLabelLang ?broader ?definition ?definitionLang ?concept ?deprecated ?exactMatch ?scopeNote ?scopeNoteLang
-WHERE {
-    ?term skos:prefLabel ?prefLabel .
-    OPTIONAL { ?term skos:altLabel ?altLabel }
-    OPTIONAL { ?term skos:altLabel ?altLabelLang . FILTER(lang(?altLabelLang) != "") }
-    OPTIONAL { ?term skos:broader ?broader }
-    OPTIONAL { ?term skos:definition ?definition }
-    OPTIONAL { ?term skos:definition ?definitionLang . FILTER(lang(?definitionLang) != "") }
-    OPTIONAL { ?term skos:concept ?concept }
-    OPTIONAL { ?term owl:deprecated ?deprecated }
-    OPTIONAL { ?term skos:exactMatch ?exactMatch }
-    OPTIONAL { ?term skos:scopeNote ?scopeNote }
-    OPTIONAL { ?term skos:scopeNote ?scopeNoteLang . FILTER(lang(?scopeNoteLang) != "") }
 }
 '
 
 # Execute the query and store results
-tic(msg = "Time to get the result:")
-envthes_data <- rdf_query(rdf = graph, query = query)
-toc()
+terms_labels <- rdf_query(graph, query_labels)
 
-envthes_data
+terms_labels
 
+### SPARQL query to retrieve terms and definition ----
+query_definition <- '
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT ?term ?definition
+WHERE {
+    ?term skos:definition ?definition .
+}
+'
+
+terms_definition <- rdf_query(graph, query_definition)
+
+### SPARQL query to retrieve terms and deprecated ----
+query_deprecated <- '
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+
+SELECT ?term ?deprecated
+WHERE {
+    ?term owl:deprecated ?deprecated .
+}
+'
+terms_deprecated <- rdf_query(graph, query_deprecated)
+
+### SPARQL query to retrieve terms and altLabel ----
+query_altLabel <- '
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT ?term ?altLabel
+WHERE {
+    ?term skos:altLabel ?altLabel .
+}
+'
+terms_altLabel <- rdf_query(graph, query_altLabel)
+
+### SPARQL query to retrieve terms and broader ----
+query_broader <- '
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT ?term ?broader
+WHERE {
+    ?term skos:broader ?broader .
+}
+'
+terms_broader <- rdf_query(graph, query_broader)
+
+### SPARQL query to retrieve terms and exactMatch ----
+query_exactMatch <- '
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT ?term ?exactMatch
+WHERE {
+    ?term skos:exactMatch ?exactMatch .
+}
+'
+terms_exactMatch <- rdf_query(graph, query_exactMatch)
+
+### SPARQL query to retrieve terms and scopeNote ----
+query_scopeNote <- '
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT ?term ?scopeNote
+WHERE {
+    ?term skos:scopeNote ?scopeNote .
+}
+'
+terms_scopeNote <- rdf_query(graph, query_scopeNote)
+
+## putting all data together ----
+envthes_data <- list(terms_labels, 
+                     terms_altLabel, 
+                     terms_broader, 
+                     terms_definition,
+                     terms_exactMatch, 
+                     terms_deprecated,
+                     terms_scopeNote) %>% 
+  reduce(full_join, by = "term")
+
+envthes_data <- envthes_data %>% 
+  filter(deprecated != TRUE | is.na(deprecated) == TRUE) # removing the deprecated
+
+# exporting the extracted data
+## saving as parquet
+write_parquet(envthes_data, here("data", "data-outputs", "envthes_data.parquet"))
+## saving as csv
+write_csv(envthes_data, here("data", "data-outputs", "envthes_data.csv"))
