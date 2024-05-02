990642afc40d4d23d9378897d59e0184e2657702
Souza Allan <allansou@lx2-870-27966.ad.helsinki.fi>
2024-05-02 15:55:32 +0300

    Adding a code that generates a R function to make semantic match of terms based on glove model.

--- code/R-semantic-mapping.R
+++ code/R-semantic-mapping.R
@@ -2,11 +2,8 @@
 source(file = here::here("code", "R-libraries.R"))
 
 # semantic mapping of terms ----
-## 2024-04-30: this is a very rough test so far, more polishing is needed 
-install.packages(c("textTinyR", "dplyr", "data.table"))
 
-library(data.table)
-
+# glove embeddings: https://nlp.stanford.edu/projects/glove/
 # Function to read GloVe embeddings
 read_glove <- function(file_path) {
   # Reading the file as a data table
@@ -22,7 +19,14 @@ read_glove <- function(file_path) {
   return(embeddings)
 }
 
+# getting the model (take several minutes)
+## DO NOT FORGET TO DELETE THE FILES AFTER USAGE (there is a code line to do that at the end of this script)
+source(here("code", "R-semantic-model-extracting.R"))
+
+# glove_embeddings <- read_glove(here("data", paste0(txt_file)))
 glove_embeddings <- read_glove(here("data", "glove.6B.300d.txt"))
+
+gc() # garbage collection
 
 # Function to convert a string to a vector representation
 string_to_vector <- function(text, embeddings) {
@@ -43,36 +47,58 @@ cosine_similarity <- function(vec1, vec2) {
   sum(vec1 * vec2) / (sqrt(sum(vec1^2)) * sqrt(sum(vec2^2)))
 }
 
-# The provided strings
-string1 <- "GEOCORDINATES DEPICTING THE LATITUDE IN WSG84"
-string2 <- "area where the sample was taken"
-string3 <- "water temperature"
-string4 <- "soil moisture"
-string5 <- "LONGITUDE IN WSG84"
-string6 <- "Leaf area index"
-string7 <- "coastal zone"
-string8 <- "Acid rain is a rain or any other form of precipitation that is unusually acidic, meaning that it possesses elevated levels of hydrogen ions. It can have harmful effects on plants, aquatic animals, and infrastructure through the process of wet deposition. Acid rain is caused by emissions of sulfur dioxide and nitrogen oxides which react with the water molecules in the atmosphere to produce acids. Governments have made efforts since the 1970s to reduce the release of sulfur dioxide into the atmosphere with positive results. Nitrogen oxides can also be produced naturally by lightning strikes and sulfur dioxide is produced by volcanic eruptions. The chemicals found in acid rain can cause paint to peel and stone statues to begin to appear old and worn down, which reduces their value and beauty."
-string9 <- "Planting of new trees and, particularly, of native plants in disturbed sites where the vegetation cover has been destroyed, to stabilize the land surface from wind and water erosion and to reclame the land for other uses. Revegetation practices are employed in mined lands, roadsides, parks, wetlands, utility corridors, riparian areas, etc."
 
-# List of candidate strings
-candidates <- list(string1, string2, string3, string4, string5, string6, string7, string8, string9)
+# making it in a function ----
+matching_terms <- function(terms_vector, target_term) {
+  # Convert the string of interest to a vector
+  vec_interest <- string_to_vector(target_term, glove_embeddings)
+  
+  # Calculate similarities
+  similarities <- sapply(terms_vector, function(cand) {
+    vec_cand <- string_to_vector(cand, glove_embeddings)
+    if (is.null(vec_cand) || is.null(vec_interest)) return(NA)
+    cosine_similarity(vec_cand, vec_interest)
+  })
+  
+  # # Finding the string with the highest similarity
+  # best_match_idx <- which.max(similarities)
+  # best_match <- terms_vector[[best_match_idx]]
+  
+  # making a tibble from the terms
+  df <- tibble(term = names(similarities), similarities)
+  
+  # processing the tibble to select the top 5 terms with higher similarity
+  df %>% 
+    filter(is.na(similarities) == FALSE) %>% 
+    arrange(-similarities) %>% 
+    slice(1:5) %>% # top 5 terms with higher similarities
+    rename(similarity = similarities)
 
-string_of_interest <- "abundance"
+}
 
-# Convert the string of interest to a vector
-vec_interest <- string_to_vector(string_of_interest, glove_embeddings)
+# testing the function ----
+## loading the list of terms from EnvThes 
+envthes_data <- read_parquet(file = here("data", "data-outputs", "envthes_data.parquet"))
 
-# Calculate similarities
-similarities <- sapply(candidates, function(cand) {
-  vec_cand <- string_to_vector(cand, glove_embeddings)
-  if (is.null(vec_cand) || is.null(vec_interest)) return(NA)
-  cosine_similarity(vec_cand, vec_interest)
-})
+### selecting only the column with the terms definitions
+candidate_terms <- envthes_data %>% 
+  # can be done by definition or by prefLabel (here is done with the term definition)
+  distinct(definition) %>% 
+  pull(definition) 
 
-# Finding the string with the highest similarity
-best_match_idx <- which.max(similarities)
-best_match <- candidates[[best_match_idx]]
+## testing the function ----
+# ### it will provide the list of the top 5 terms with higher similarity
+# matching_terms(terms_vector = candidate_terms, target_term = "fire")
+# 
+# ## comparing with the input candidates and checking the recommended term
+# envthes_data %>% 
+#   filter(definition == "An unplanned, unwanted wildland fire") %>% # replace the term here
+#   distinct(term, prefLabel, definition) %>% 
+#   pull(prefLabel)
 
-cat("The string that best matches the string of interest is:", best_match, "\n")
-
-similarities
+# removing the large txt file from the semantic model ----
+# file.remove(paste0(here("data"), "/", txt_file))
+file.remove(paste0(here("data"), "/", "glove.6B.50d.txt"))
+file.remove(paste0(here("data"), "/", "glove.6B.100d.txt"))
+file.remove(paste0(here("data"), "/", "glove.6B.200d.txt"))
+file.remove(paste0(here("data"), "/", "glove.6B.300d.txt"))
