ca2d9eefe61f5526d942c6cf09d2c328647fe759
Souza Allan <allansou@lx2-870-27966.ad.helsinki.fi>
2024-05-02 17:23:48 +0300

    Adding the testing of the custom made function to match terms using semantics.

--- code/R-data-processing.R
+++ code/R-data-processing.R
@@ -13,3 +13,29 @@ ds_source <- read_excel(here("data", "HCSupplementalTable3_FullTermList_r2_v4_RW
 ds_source %>% 
   glimpse()
 
+# trying to map terms from EnvThes ----
+source(file = here::here("code", "R-semantic-mapping.R"))
+
+## loading the list of terms from EnvThes 
+envthes_data <- read_parquet(file = here("data", "data-outputs", "envthes_data.parquet"))
+
+### selecting only the column with the terms definitions
+envthes_candidate_terms <- envthes_data %>% 
+  # can be done by definition or by prefLabel (here is done with the term definition)
+  distinct(definition) %>% 
+  pull(definition) 
+
+# running the custom made function to look up for terms with closer semantic meaning
+## the function provides the ordered terms with higher similarities with the target_term
+matching_terms(terms_vector = envthes_candidate_terms, target_term = "fire")
+
+# trying to map terms from DwC ----
+## loading the list of terms from Darwin Core 
+dwc_data <- read_csv("data/darwin_core_term_versions.csv")
+
+### selecting only the column with the terms definitions
+dwc_candidate_terms <- dwc_data %>% 
+  pull(definition)
+
+matching_terms(terms_vector = dwc_candidate_terms, target_term = "sampling")
+
